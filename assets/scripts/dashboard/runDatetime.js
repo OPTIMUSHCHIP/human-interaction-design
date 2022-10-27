b15dcacf8f1590a5615a2deca94662dab9c5ca20
khanhdeptrai <tqk792001@gmail.com>
2022-10-27 11:06:43 +0700

    Update map

--- assets/scripts/dashboard/runDatetime.js
+++ assets/scripts/dashboard/runDatetime.js
@@ -1,4 +1,4 @@
-const datetimeText = document.querySelector(".datetime-text");
+const datetimeText = $(".datetime-text");
 
 const getDateTime = () => {
   let DATETIME = "";
@@ -37,39 +37,8 @@ const formatTime = (time) => {
   return time;
 };
 
-// Run timer
+// Append datetime
 setInterval(() => {
   const DATETIME_TEXT = getDateTime();
   datetimeText.textContent = DATETIME_TEXT;
 }, 1000);
-
-// Start skiing timer
-const startCounterBtn = document.querySelector(".start-counter");
-// startCounterBtn.onclick = handleStartTimer;
-
-const handleStartTimer = () => {
-  let TIMER = setInterval(clock, 1000);
-  var msec = 00;
-  var sec = 00;
-  var min = 00;
-
-  function clock() {
-    msec += 1;
-    if (msec == 60) {
-      sec += 1;
-      msec = 00;
-      if (sec == 60) {
-        sec = 00;
-        min += 1;
-        if (sec % 2 == 0) {
-          alert("Pair");
-        }
-      }
-    }
-    TIMER = `${min}:${sec}:${msec}`;
-  }
-
-  return TIMER;
-};
-
-console.log(handleStartTimer());
---
+++ assets/scripts/index.js
@@ -0,0 +1,24 @@
+const $ = document.querySelector.bind(document);
+
+const currentUrl = window.location.pathname.slice(7);
+const dashboard = $("li.dashboard"),
+  map = $("li.map"),
+  history = $("li.history");
+
+const handleActiveNavItem = (url) => {
+  if (url === "dashboard.html") {
+    dashboard.classList.add("active");
+    map.classList.remove("active");
+    history.classList.remove("active");
+  } else if (url === "map.html") {
+    map.classList.add("active");
+    dashboard.classList.remove("active");
+    history.classList.remove("active");
+  } else if (url === "history") {
+    history.classList.add("active");
+    dashboard.classList.remove("active");
+    map.classList.remove("active");
+  }
+};
+
+window.onload = handleActiveNavItem(currentUrl);
